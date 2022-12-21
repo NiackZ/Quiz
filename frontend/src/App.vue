c65768d7640045bb2e6d67685abec54cf0b3edbc
NiackZ <niack@mail.ru>
2022-12-21 18:37:30 +0500

    Front template

---
+++ frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <MyHeader/>
+  <input type="text" name="msgText" v-model="msg">
+  <div>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+import MyHeader from "@/components/header/MyHeader";
+export default {
+  components: {MyHeader},
+  data() {
+    return {
+      msg: "Hello"
+    }
+  }
+}
+</script>
+
+<style>
+*{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ frontend/src/components/button/MyButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button>
+    {{text}}
+  </button>
+</template>
+
+<script>
+export default {
+  props:{
+    text:{
+      type: String,
+      require: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
