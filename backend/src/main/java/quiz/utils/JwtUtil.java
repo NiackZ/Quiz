472009c234a766e74eb9b6c39fc89fd80eb74e9f
NiackZ <niack@mail.ru>
2023-07-12 00:36:26 +0500

    Класс для генерации jwt

--- backend/src/main/java/quiz/utils/JwtUtil.java
+++ backend/src/main/java/quiz/utils/JwtUtil.java
@@ -1,13 +1,13 @@
 package quiz.utils;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 
 import java.time.Duration;
 import java.util.Date;
