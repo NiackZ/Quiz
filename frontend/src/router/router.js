635b5518bf78d1be45de7bbf82670f245c22a91c
NiackZ <niack@mail.ru>
2023-10-14 13:24:59 +0500

    Добавлены мета данные для роута

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -2,8 +2,11 @@ import {createRouter, createWebHistory} from "vue-router";
 import Main from "../pages/Main.vue";
 import Admin from "../pages/Admin/Admin.vue";
 import CreateTitle from "../pages/Admin/CreateTitle.vue";
-import Item from "../pages/Content/Item.vue";
+import Item from "../pages/Public/Content/Item.vue";
 import Profile from "../pages/User/Profile.vue";
+import Error403 from "../pages/Error/Error403.vue";
+import Login from "../pages/Public/Login.vue";
+import {LOGIN_PATH} from "../constants/constants.js";
 
 const routes = [
     {
@@ -12,11 +15,17 @@ const routes = [
     },
     {
         path: '/admin',
-        component: Admin
+        component: Admin,
+        meta: {
+            requiresAuth: true
+        }
     },
     {
         path: '/admin/create/title',
-        component: CreateTitle
+        component: CreateTitle,
+        meta: {
+            requiresAuth: true
+        }
     },
     {
         path: '/item/:id',
@@ -24,7 +33,21 @@ const routes = [
     },
     {
         path: '/profile',
-        component: Profile
+        component: Profile,
+        meta: {
+            requiresAuth: true
+        }
+    },
+    {
+        path: '/403',
+        component: Error403
+    },
+    {
+        path: LOGIN_PATH,
+        component: Login,
+        meta: {
+            requiresAuth: false
+        }
     }
 ]
 
