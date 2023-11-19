049b9f3a32240390f082f2572d5511b5e1a8e941
NiackZ <niack@mail.ru>
2023-11-19 17:30:54 +0500

    Добавлены функции для получения списков

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -3,3 +3,19 @@ import axios from '/src/axios/http-common'
 export async function checkRights(userId, rights) {
     return await axios.post('/rights/check-rights', {userId, rights});
 }
+
+export async function getTypes() {
+    return await axios.get('/types');
+}
+
+export async function getStudios() {
+    return await axios.get("/studios");
+}
+
+export async function getGenres() {
+    return await axios.get("/genres");
+}
+
+export async function getStatuses() {
+    return await axios.get("/statuses");
+}
