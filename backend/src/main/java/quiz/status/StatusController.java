40de8b1f264accaf611d486d254d866bab29491a
NiackZ <niack@mail.ru>
2023-11-19 17:30:00 +0500

    Фикс пути контролера

--- backend/src/main/java/quiz/status/StatusController.java
+++ backend/src/main/java/quiz/status/StatusController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/studios")
+@RequestMapping("/statuses")
 @AllArgsConstructor
 public class StatusController {
 
